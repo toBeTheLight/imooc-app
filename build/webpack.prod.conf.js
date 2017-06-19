c8ad23ff76cf60b954220398b09cbbcf8ea9fd1b
toBeTheLight <yl1991web@126.com>
2017-06-19 16:15:11 +0800

    delete:copyWebpackPlugin

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -88,13 +88,13 @@ var webpackConfig = merge(baseWebpackConfig, {
       chunks: ['vendor']
     }),
     // copy custom static assets
-    new CopyWebpackPlugin([
-      {
-        from: path.resolve(__dirname, '../static'),
-        to: config.build.assetsSubDirectory,
-        ignore: ['.*']
-      }
-    ])
+    // new CopyWebpackPlugin([
+    //   {
+    //     from: path.resolve(__dirname, '../static'),
+    //     to: config.build.assetsSubDirectory,
+    //     ignore: ['.*']
+    //   }
+    // ])
   ]
 })
 
